76ebfced0749dc927ae5af7d4e045c18e98d951f
Garima Malhotra <garimam@derecho7.hsn.de.hpc.ucar.edu>
2026-06-30 12:33:36 -0600

    Add Derecho build support for FV3-WAM
    - modulefiles/ufs_derecho.intel.lua: spack-stack 1.9.2 (oneapi) + ufs_common;
      previous version kept as ufs_derecho.intelold.lua
    - tests/fv3_conf/control_wam_run.IN: Derecho input data paths
    - FV3: unified_ugwp drag_suite_psl build fix
    - point FV3/ccpp-physics submodules at personal forks for standalone build

--- modulefiles/ufs_derecho.intel.lua
+++ modulefiles/ufs_derecho.intel.lua
@@ -1,30 +1,25 @@
 help([[
-loads UFS Model prerequisites for NOAA Parallelworks/Intel
+loads UFS Model prerequisites for Derecho/Intel
 ]])
 
-setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles")
-load("ecflow/5.8.4")
-load("mysql/8.0.33")
+local ss_root = "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles"
 
-setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/modulefiles_extra")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.6.0/envs/upp-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", pathJoin(ss_root, "Core"))
 
-unload("ncarcompilers")
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
-stack_cray_mpich_ver=os.getenv("stack-cray-mpich_ver") or "8.1.25"
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.10.13"
-load(pathJoin("stack-python", stack_python_ver))
+-- scotch is under the gcc toolchain in this env
+prepend_path("MODULEPATH", pathJoin(ss_root, "cray-mpich/8.1.29-3sepg3g/gcc/12.4.0"))
+
+load("ufs_common")
 
 setenv("CMAKE_Platform", "derecho.intel")
-load("ufs-weather-model-env")
 
 whatis("Description: UFS build environment")
